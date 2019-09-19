50800adcb9720f36844eefe378248e01395efce1
ioay <oguzhan.ay@betek.com.tr>
2019-09-19 09:56:42 +0300

    Movies Detail Added.

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
 
 export enum SearchType {
   all = '',
@@ -13,24 +13,20 @@ export enum SearchType {
 @Injectable({
   providedIn: 'root'
 })
-export class MovieService {
 
+export class MovieService {
   url = 'http://www.omdbapi.com/';
-  apiKey = '2be398c0';
+  apiKey = '2be398c0'; // <-- Enter your own key here!
 
   constructor(private http: HttpClient) { }
 
-  searchData(title: string, type: SearchType): Observable <any> {
-      return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`)
-      .pipe(
-        map(results => {
-          console.log('RAW: ', results);
-          return results['Search'];
-        })
-      );
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`).pipe(
+      map(results => results['Search'])
+    );
   }
 
   getDetails(id) {
-    return this.http.get(`${this.url}?i={id}&plot=full&apikey=${this.apiKey}`);
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
   }
 }
